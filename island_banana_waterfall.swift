652ac3612885bcee4eb5d25f12bcb47826a2d8d9
liliatalak <136699097+liliatalak@users.noreply.github.com>
2023-06-21 14:14:16 +0700

    Merge pull request

---
+++ island_banana_waterfall.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+//MARK: - Crime Prevention
+
+//1. Make sure to always lock your doors and windows of your home when you are away
+func lockDoorsAndWindows() {
+    print("Always remember to lock your doors and windows whenever you leave the house, even if you're just stepping out for a few minutes")
+}
+
+//2. Install a home security system 
+func installSecuritySystem() {
+   print("A home security system is one of the best forms of protection for your home. It provides a level of protection that is nearly impossible to achieve with other methods")
+}
+
+//3. Install motion-sensing lights around the perimeter of your home
+func installMotionSensingLights() {
+    print("Motion sensing lights are an inexpensive but effective security measure. When the lights are tripped, they will deter potential criminals from approaching your house")
+}
+
+//4. Ensure exterior doors are made of sturdy materials
+func sturdyDoorMaterials() {
+    print("Choose durable materials for your exterior doors such as solid wood or metal and ensure that they are fitted with a deadbolt lock to add another layer of protection")
+}
+
+//5. Keep hedges and trees around your home trimmed
+func trimTreesAndHedges() {
+    print("Trim any large hedges and trees around the perimeter of your home to reduce hiding places for criminals or away for them to enter your home undetected")
+}
+
+//6. Add a security camera
+func addSecurityCamera() {
+    print("Security cameras are a great way to deter criminals and keep an eye on your home after dark. Position them strategically around the perimeter of your house for the best protection")
+}
+
+//7. Use window locks
+func useWindowLocks() {
+    print("Window locks are an easy and inexpensive way to secure your home. Install them on all windows, especially on the ground floor, to make it harder for intruders to break in")
+}
+
+//8. Get to know your neighbors
+func getToKnowNeighbors() {
+    print("Getting to know the people who live around you is a great way to protect your home. Knowing your neighbors will make it easier to spot suspicious activity in the area")
+}
+
+//9. Don't leave ladders or tools around
+func removeLaddersAndTools() {
+    print("Never leave ladders or tools around your home. These can give criminals access to your roof or upper floors, making it easier to break in")
+}
+
+//10. Install motion-sensing alarms
+func installMotionSensingAlarms() {
+    print("Motion-sensing alarms are an effective way to deter criminals. They will sound an alarm when they detect movement, alerting you and your neighbors to any suspicious activity")
+}
